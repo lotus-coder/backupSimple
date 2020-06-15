70c97719c8715edbc6e879c3774d6419a41e5f5c
lotus-coder <lotuflas@gmail.com>
2020-06-15 17:51:04 +0200

    script simple de python para hacer un backup

---
+++ backupSimple.py
@@ -0,0 +1,60 @@
+import os
+import shutil
+class Backup:
+    def __init__(self,rutaOrigen,rutaDestino):
+        self.rutaOrigen = rutaOrigen
+        self.rutaDestino = rutaDestino
+        self.cuantosNiveles = rutaOrigen.count('\\')
+    
+    def dameRuta(self):
+        return (self.rutaOrigen)
+        
+    def dameArchivos(self,ruta):
+        todosLosArchivos = []
+        for nombreCarpeta, subCarpetas, nombreArchivos in os.walk(ruta):
+            for subCarpeta in subCarpetas:
+                todosLosArchivos.append(nombreCarpeta+'\\'+subCarpeta)
+            for archivo in nombreArchivos:
+                todosLosArchivos.append(nombreCarpeta+'\\'+archivo)
+        return todosLosArchivos
+
+    def enseniaArchivos(self):
+        for nombreCarpeta, subCarpetas, nombreArchivos in os.walk(self.rutaOrigen):
+            print('The current folder is ' + nombreCarpeta)
+            for subCarpeta in subCarpetas:
+                print('SUBFOLDER OF ' + nombreCarpeta + ': ' + subCarpeta)
+            for archivo in nombreArchivos:
+                print('FILE INSIDE ' + nombreCarpeta + ': '+ archivo)
+            print('')
+
+    def copiaTodo(self):
+        origen = self.dameArchivos(self.rutaOrigen)
+        destino = self.dameArchivos(self.rutaDestino)
+        num=0
+        print(self.cuantosNiveles)
+        if destino!=origen:
+            for i in origen:
+                rutaNueva = self.rutaDestino + origen[num][len(self.rutaOrigen):]
+                texto = str(origen[num])
+                if os.path.isfile(i):
+                    print(origen[num][len(self.rutaOrigen):])
+                    print('Esto es Origen:  '+origen[num][origen[num].index(self.rutaOrigen):])
+                    print('Esto es Destino: '+rutaNueva)
+                    if not os.path.exists(rutaNueva):
+                        shutil.copyfile(i, rutaNueva)
+                else:
+                    print(origen[num][len(self.rutaOrigen):])
+                    print('Esto es Origen:  '+origen[num][origen[num].index(self.rutaOrigen):])
+                    print('Esto es Destino: '+rutaNueva)
+                    if not os.path.exists(rutaNueva):
+                        os.makedirs(rutaNueva)
+                num+=1
+
+
+def main():
+    mimimi = Backup("/una/ruta/absoluta",'/otra/ruta/absoluta')
+    mimimi.copiaTodo()
+
+    
+if __name__ =='__main__':
+    main()
